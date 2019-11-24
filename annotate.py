f8ddeb437f1e7b1fe9c30dd5ed38e07b90cec9c5
Arkhelyi <arkhelyi@gmail.com>
2019-11-24 15:35:02 +0800

    clean-up version

---
+++ annotate.py
@@ -0,0 +1,282 @@
+"""Library + CLI to annotate FFXIV in-game map assets with Elite Marks spawn positions.
+
+Copyright @ Arkhelyi, 2019"""
+
+from collections import defaultdict
+import inspect
+from operator import itemgetter
+import os
+import pathlib
+import shutil
+import subprocess
+import yaml
+
+import fire
+import pyperclip
+from PIL import Image, ImageDraw
+
+from helpers import ZoneApi, MarksHelper, Position, drop_shadow, m2c, Legend
+
+
+class MapAnnotator:
+    """Library + CLI to annotate FFXIV in-game map assets with Elite Marks spawn positions.
+    
+    To perform this, it relies on 3 configuration files:
+    - data/zone_info.yaml: file generated from xivapi mapping a zone name to its region, map filename, scale factor
+    - data/marks.json: static data file mapping a mark to its rank, zone and spawn positions
+    - data/config.yaml: configuration for paths, marker style, legend style and position
+    
+    In addition to the python requirements, it requires ImageMagick to be installed (path can be in
+    user $PATH or provided in configuration)"""
+
+    def __init__(self):
+        with open("data/config.yaml", "rt") as fp:
+            a = yaml.load_all(fp, Loader=yaml.Loader)
+            self._config = {k: v for i in a for k, v in i.items()}
+
+        self._base_path = pathlib.Path(self._config["tool"]["textools_path"])
+        self._project_path = pathlib.Path(self._config["tool"]["project_path"])
+        self._magickpath = self._config["tool"]["imagemagick_path"] or shutil.which(
+            "magick"
+        )
+
+        zones = self._config["zones"]
+        ZoneApi(zones.keys()).load_zone_info(zones)
+        self._zones = zones
+        self._Mark, self._marks = MarksHelper.load_marks("data/marks.json")
+        self._iscli = inspect.stack()[-2].function == "Fire"
+
+    def _get_path(self, name, project=False, backup=False, ext=None):
+        base = self._base_path if not project else self._project_path
+        region = self._zones[name]["region"]
+        file = self._zones[name]["filename"]
+        bck = "_backup" if backup else ""
+        ext = ext or "dds"
+        return (base / region / name / (file + "_m" + bck)).with_suffix("." + ext)
+
+    def _get_zone_marks(self, zone, rank_remap=False):
+        marks = {
+            mark.name: (mark.rank, mark.spawns)
+            for mark in self._marks
+            if mark.zone == zone
+        }
+
+        def remap_rank(lmarks, rank):
+            """Given a dictionary {mark_name: mark_rank}, will rewrite the values for the given
+            rank to account for multiple occurences of it.
+            Example: A -> A1, A2"""
+            fmarks = [k for k, v in lmarks.items() if v[0] == rank]
+            for i, m in enumerate(sorted(fmarks)):
+                lmarks[m] = (f"{rank}{i+1}", lmarks[m][1])
+
+        if rank_remap:
+            remap_rank(marks, "A")
+            remap_rank(marks, "B")
+        return marks
+
+    def check_files(self, backup=False):
+        """Verify the presence of asset backup files (used as source for map annotation)."""
+
+        for name in self._zones:
+            path = self._get_path(name, backup=backup)
+            if not path.exists():
+                print(f"MISSING: file '{name}' @ '{path}'")
+        print("File check complete.")
+
+    def check_spawn_points(self, threshold=0.5):
+        """List all spawn points that are closer to each other than `threshold` (default 0.5y)"""
+
+        def distance(s1, s2):
+            from math import sqrt
+
+            return sqrt((s2[0] - s1[0]) ** 2 + (s2[1] - s1[1]) ** 2)
+
+        suspicious = defaultdict(list)
+
+        for zone in self._zones:
+            spawnset = defaultdict(list)
+            marks = self._get_zone_marks(zone)
+            for mark, (_, spawns) in marks.items():
+                for spawn in spawns:
+                    spawnset[tuple(spawn)].append(mark)
+
+            spawn_list = list(spawnset.keys())
+            for i1, spawn1 in enumerate(spawn_list):
+                for spawn2 in spawn_list[i1 + 1 :]:
+                    if distance(spawn1, spawn2) <= threshold:
+                        suspicious[zone].append([spawn1, spawn2])
+
+        return suspicious
+
+    def backup_files(self, warning=True):
+        """Backup asset export files.
+
+        Backup files are used as source to draw annotations so they *need* to be the original files.
+        The annotation process replaces the original export, so calling this function after some maps
+        have been annotated will result in already annotated source files and the original assets will
+        need to be re-exported with TexTools and backed-up again.
+        
+        Call this method with warning=False to execute."""
+        if warning:
+            print(
+                "Be careful! This command should only be used after a fresh export of original in-game assets."
+            )
+            print(
+                "Otherwise, you will overwrite your backups and need to re-export original assets before annotating maps."
+            )
+            print("Call this method with warning=False to execute.")
+            return
+        self.check_files()
+        for name in self.zone_info:
+            path = self._get_path(name)
+            bpath = self._get_path(name, backup=True)
+            shutil.copy(path, bpath)
+
+    def annotate_map(self, name, save=False):
+        """Annotate the map of the zone `name`. Optionally save the modified asset file and its png preview
+        
+        Saves are made both in the TexTools folder for easy import and to the map project folder for repo update."""
+        map_layer = Image.open(self._get_path(name, backup=True))
+
+        marker_layer = Image.new("RGBA", map_layer.size, color=(0, 0, 0, 0))
+
+        scale = self._zones[name]["scale"]
+        zone_marks = self._get_zone_marks(name, True)
+        spawns = defaultdict(dict)
+        for mark, (rank, spots) in zone_marks.items():
+            for p in spots:
+                spawns[tuple(p)][mark] = rank
+        for spawn, marks in spawns.items():
+            screen_position = Position(m2c(spawn[0], scale), m2c(spawn[1], scale))
+            self._draw_marker(marker_layer, screen_position, marks)
+
+        marker_layer = drop_shadow(
+            marker_layer,
+            offset=Position(*self._config["marker"]["shadow_offset"]),
+            shadow_color=self._config["marker"]["shadow_color"],
+            iterations=self._config["marker"]["shadow_iterations"],
+            scale=self._config["marker"]["shadow_scale"],
+            direction=self._config["marker"]["shadow_direction"],
+        )
+
+        marks = {name: rank for name, (rank, _) in zone_marks.items()}
+        new_map = Image.alpha_composite(map_layer, marker_layer)
+        legend_rows = self._zones[name]["legend"]["rows"]
+        legend_position = Position(*self._zones[name]["legend"]["position"])
+        complete_map = self._draw_legend(new_map, marks, legend_rows, legend_position)
+
+        if save:
+            self._save_map(complete_map, name)
+        if self._iscli:
+            complete_map.show(title=name)
+            return
+        return complete_map
+
+    def _draw_legend(self, img, marks, rows, position):
+        legend = Legend(self._config).draw(img.size, position, marks, rows)
+        return Image.alpha_composite(img, legend)
+
+    def _draw_marker(self, img, position, marks):
+        draw = ImageDraw.Draw(img)
+
+        size = self._config["marker"]["size"]
+        inner_size = size * self._config["marker"]["inner_size_scale"]
+        colors = self._config["colors"]
+        box = (*(position - 0.5 * size), *(position + 0.5 * size))
+        inner_box = (*(position - 0.5 * inner_size), *(position + 0.5 * inner_size))
+
+        angle = 180
+        for angle, rank in zip(range(180, 180 + 360, 90), ["B1", "B2", "A1", "A2"]):
+            if rank in marks.values():
+                draw.pieslice(
+                    box, angle, angle + 90, fill=colors[rank], outline=None, width=0
+                )
+            angle += 90
+
+        if "S" in marks.values():
+            draw.ellipse(inner_box, fill=colors["S"], outline=None, width=0)
+
+        if "SS" in marks.values() or "SSs" in marks.values():
+            rank = next(
+                iter(marks.values())
+            )  # There should be only one rank if it's SS or SSs
+            draw.ellipse(box, fill=colors[rank], outline=None, width=0)
+
+        return img
+
+    def _save_map(self, img, name):
+        src = self._get_path(name, ext="bmp")
+        dst = src.with_suffix(".dds")
+        img.save(src, format="bmp")
+        cmd = f'{self._magickpath} convert -define dds:compression=dxt1 -define dds:mipmaps=0 "{src}" "{dst}"'
+        subprocess.run(cmd, capture_output=True)
+        src.unlink()
+        pdst = self._get_path(name, ext="dds", project=True)
+        os.makedirs(os.path.dirname(pdst), exist_ok=True)
+        shutil.copy(dst, pdst)
+
+        preview_dst = pdst.with_suffix(".png")
+        img.save(preview_dst, format="png")
+
+    def annotate_all(self):
+        """Annotate and save all maps.
+
+        Saves are made both in the TexTools folder for easy import and to the map project folder for repo update."""
+        for zone in self._zones:
+            self.fix_map(zone, save=True)
+
+    def generate_thumbnail_table(self):
+        """Generate html code for the collapsable preview tables used in the map repo's README.
+
+        Results is copied into the OS' clipboard"""
+        document = ""
+        for expansion, expac_name in self._config["expansions"].items():
+            expac_data = defaultdict(list)
+            for zone, info in self._zones.items():
+                if info["expansion"] == expansion:
+                    if info["region"] == "Norvrandt":
+                        if len(expac_data["Norvrandt 1"]) < 3:
+                            expac_data["Norvrandt 1"].append((zone, info["filename"]))
+                        else:
+                            expac_data["Norvrandt 2"].append((zone, info["filename"]))
+                    else:
+                        expac_data[info["region"]].append((zone, info["filename"]))
+
+            for zones in expac_data.values():
+                zones.sort(key=itemgetter(0))
+
+            n_rows = max([len(v) for v in expac_data.values()])
+
+            url_template = self._config["tool"]["preview_url_template"]
+            item_template = '<a href="{url}"><img src="{url}" width="{w}"/>'
+            header_template = ":---: | "
+            title = "| "
+            header = "| "
+
+            summary = f"<details><summary><b>{expac_name}</b></summary>\n\n"
+            title = summary + title
+            for region in sorted(expac_data.keys()):
+                title += f"{region} | "
+                header += header_template
+                table = title + "\n" + header + "\n"
+
+            for i in range(n_rows):
+                line = "| "
+                for region in sorted(expac_data.keys()):
+                    try:
+                        zone, file = expac_data[region][i]
+                        if "Norvrandt" in region:
+                            region = region[:-2]
+                        url = url_template.format(region=region, zone=zone, file=file)
+                        item = item_template.format(url=url, w=150)
+                        line += f"{item} |"
+                    except IndexError:
+                        line += "   | "
+                table += line + "\n"
+            table += "\n</details>\n\n"
+            document += table
+        pyperclip.copy(document)
+
+
+if __name__ == "__main__":
+    fire.Fire(MapAnnotator)
---
+++ helpers.py
@@ -0,0 +1,394 @@
+from collections import namedtuple
+from copy import deepcopy
+import json
+import yaml
+from math import pi, cos, sin
+from operator import itemgetter
+import re
+
+from PIL import Image, ImageFilter, ImageFont, ImageDraw, ImageColor
+import requests
+
+
+def yml_tuple_constructor(loader, node):
+    # from https://stackoverflow.com/a/48452275t
+    # this is to convert the string written as a tuple into a python tuple
+    # this little parse is really just for what I needed, feel free to change it!
+    def parse_tup_el(el):
+        # try to convert into int or float else keep the string
+        if el.isdigit():
+            return int(el)
+        try:
+            return float(el)
+        except ValueError:
+            return el
+
+    value = loader.construct_scalar(node)
+    # remove the ( ) from the string, strip spaces and split into elements
+    tup_elements = value[1:-1].replace(" ", "").split(",")
+    # remove the last element if the tuple was written as (x,b,)
+    if tup_elements[-1] == "":
+        tup_elements.pop(-1)
+    tup = tuple(map(parse_tup_el, tup_elements))
+    return tup
+
+
+# !tuple is my own tag name, I think you could choose anything you want
+yaml.add_constructor("!tuple", yml_tuple_constructor)
+# this is to spot the strings written as tuple in the yaml
+pattern = re.compile(r"\(([+-]?([0-9]*[.])?[0-9]+, ?)+([+-]?([0-9]*[.])?[0-9]+)?\)")
+yaml.add_implicit_resolver("!tuple", pattern)
+
+
+class MarksHelper:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def dump_marks(marklist, filename):
+        new_list = [t._asdict() for t in marklist]
+        if filename == "str":
+            return json.dumps(new_list)
+        with open(filename, "wt") as fp:
+            json.dump(new_list, fp)
+
+    @staticmethod
+    def load_marks(filename):
+        with open(filename, "rt") as fp:
+            marks = json.load(fp)
+
+        Mark = namedtuple("Mark", marks[0])
+        return Mark, [Mark(**mark) for mark in marks]
+
+    @staticmethod
+    def sort_marks(filename):
+        with open(filename, "rt") as fp:
+            marks = json.load(fp)
+
+        marks.sort(key=itemgetter("zone", "rank", "name"))
+        for mark in marks:
+            mark["spawns"] = sorted(mark["spawns"], key=itemgetter(0, 1))
+        with open("new_" + filename, "wt") as fp:
+            json.dump(marks, fp)
+
+
+class ZoneApi:
+    def __init__(self, zones):
+        self.base_url = "https://xivapi.com"
+        self.zones = zones
+        self.cachename = "data/zone_info"
+
+    def _get_zone_url(self, name):
+        resp = requests.get(f"{self.base_url}/search?indexes=PlaceName&string={name}")
+        if resp.ok:
+            results = resp.json()["Results"]
+            candidates = []
+            for res in results:
+                if res["Name"] == name:
+                    candidates.append(res)
+            if len(candidates) > 1:
+                if name == "Mor Dhona":
+                    return next((item for item in candidates if item["ID"] == 26))[
+                        "Url"
+                    ]
+                raise Exception(candidates)
+            return candidates[0]["Url"]
+        else:
+            raise Exception(resp)
+
+    def get_zone_info(self, name):
+        zone_url = self._get_zone_url(name)
+        resp = requests.get(f"{self.base_url}{zone_url}")
+        if resp.ok:
+            results = resp.json()["Maps"][0]
+            return results
+        else:
+            raise Exception(resp)
+
+    def get_zone_data(self, info):
+        region = info["PlaceNameRegion"]["Name"]
+        size_factor = info["SizeFactor"]
+        filename = info["MapFilenameId"].replace("/", "")
+        return {"region": region, "scale": size_factor, "filename": filename}
+
+    def get_all_zone_info(self):
+        results = {}
+        for zone in self.zones:
+            info = self.get_zone_info(zone)
+            results[zone] = self.get_zone_data(info)
+        return results
+
+    def save_zone_info(self, zones, as_json=False):
+        if as_json:
+            with open(self.cachename + ".json", "wt") as fp:
+                json.dump(zones, fp)
+                return
+        with open(self.cachename + ".yaml", "wt") as fp:
+            yaml.safe_dump(zones, fp)
+
+    def load_zone_info(self, zones=None):
+        with open(self.cachename + ".yaml", "rt") as fp:
+            info = yaml.load(fp, Loader=yaml.Loader)
+        if zones:
+            for zone in list(zones.keys()):
+                zones[zone].update(info[zone])
+            return
+        return info
+
+
+class Position:
+    """Position class that represents a 2-tuple of coordinates and provide helpful functions to operate on those.
+
+    Accessing 'other''s values is done through indexers in order to be compatible with coordinates passed as lists or tuples."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        try:
+            return Position(self.x + other[0], self.y + other[1])
+        except TypeError:
+            try:
+                return Position(self.x + other, self.y + other)
+            except TypeError:
+                raise "Position expect to be added to a Position-like or a scalar"
+
+    def __radd__(self, other):
+        return self + other
+
+    def __mul__(self, other):
+        try:
+            return Position(self.x * other[0], self.y * other[1])
+        except TypeError:
+            try:
+                return Position(self.x * other, self.y * other)
+            except TypeError:
+                raise "Position expect to be multiplied by a Position-like or a scalar"
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __neg__(self):
+        return Position(-self.x, -self.y)
+
+    def __sub__(self, other):
+        try:
+            return Position(self.x - other[0], self.y - other[1])
+        except TypeError:
+            try:
+                return Position(self.x - other, self.y - other)
+            except TypeError:
+                raise "Position expect to be substracted from a Position-like or a scalar"
+
+    def __rsub__(self, other):
+        return self.__neg__() + other
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.x
+        if index == 1:
+            return self.y
+
+    def __setitem__(self, index, value):
+        if index == 0:
+            self.x = value
+        if index == 1:
+            self.y = value
+
+    def __iter__(self):
+        return (self.__dict__[item] for item in sorted(self.__dict__))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.x},{self.y})"
+
+
+def compute_columns(n_items, n_rows):
+    """Compute the required number of columns given a number of items n_items to be displayed in a grid n_rows x n_cols"""
+    if n_rows > n_items:
+        return n_items, 1
+    d = n_items // n_rows
+    n_cols = d + (1 if n_items % n_rows else 0)
+    return n_rows, n_cols
+
+
+def m2c(pos, scale=100):
+    """Convert map coordinates to screen/pixel coordinates"""
+    return round((pos - 1) * scale * 0.01 / 40.85 * 2048)
+
+
+def c2m(pos, scale=100):
+    """Convert screen/pixel coordinates to map coordinates"""
+    return pos * 40.85 * 100 / 2048 / scale + 1
+
+
+def drop_shadow(img, offset, shadow_color, iterations=5, scale=1, direction=None):
+    alpha = img.getchannel("A")
+
+    base_shadow = Image.new("RGBA", img.size, color=shadow_color)
+    base_shadow.putalpha(alpha)
+
+    for i in range(iterations):
+        base_shadow = base_shadow.filter(ImageFilter.BLUR)
+
+    if direction == "radial":
+        radial_steps = 4
+        radial_angle_shift = 2 * pi / radial_steps
+        x, y = -offset.x, -offset.y
+        shadow = base_shadow.transform(
+            base_shadow.size, Image.AFFINE, (scale, 0, x, 0, scale, y)
+        )
+        for theta in [i * radial_angle_shift for i in range(1, 4)]:
+            x, y = x * cos(theta) - y * sin(theta), x * sin(theta) + y * cos(theta)
+            temp_shadow = base_shadow.transform(
+                base_shadow.size, Image.AFFINE, (scale, 0, x, 0, scale, y)
+            )
+            shadow = Image.alpha_composite(temp_shadow, shadow)
+    else:
+        shadow = base_shadow.transform(
+            base_shadow.size, Image.AFFINE, (scale, 0, -offset.x, 0, scale, -offset.y)
+        )
+    return Image.alpha_composite(shadow, img)
+
+
+class Legend:
+    def __init__(self, config):
+        self.inner_offset = Position(
+            *config["legend"]["inner_offset"]
+        )  # distance between legend border and legend items
+        self.shadow_offset = Position(
+            *config["legend"]["shadow_offset"]
+        )  # offset to shift the shadow with
+        self.column_space = config["legend"]["column_spacing"]  # Space between colum
+        self.line_space = config["legend"][
+            "line_spacing"
+        ]  # Space between rows of items
+        self.mark_scale = config["legend"][
+            "mark_scale"
+        ]  # Percentage by which we scale the marker to the text height
+        self.font_stroke = config["legend"]["font_stroke"]  # font outline size
+        self.space = config["legend"][
+            "border_space"
+        ]  # spacing between the two rectangles forming the border
+        self.font = ImageFont.truetype(
+            config["legend"]["font"], config["legend"]["font_size"]
+        )
+        self.shadow_color = config["legend"]["shadow_color"]
+        self.shadow_iterations = config["legend"]["shadow_iterations"]
+        self.colors = config["colors"]
+
+    def draw(self, img_size, position, marks, rows):
+        img = Image.new("RGBA", img_size, color=(0, 0, 0, 0))
+
+        position = Position(*position)  # ensure Position object
+        inner_position = position + self.inner_offset
+        n_items = len(marks)
+        rows, columns = compute_columns(n_items, rows)
+        max_height = self._check_height(img, marks)
+
+        # Initialize the size of the legend. Height is already determined but width will be updated as we draw.
+        size = Position(
+            (columns - 1) * self.column_space,
+            rows * max_height + (rows - 1) * self.line_space,
+        )
+
+        current_position = deepcopy(inner_position)
+        max_width = 0
+        grid_list = [
+            (r, c) for c in range(1, columns + 1) for r in range(1, rows + 1)
+        ]  # this is a list of grid coordinates
+        for grid_pos, (mark, rank) in zip(grid_list, marks.items()):
+            if mark:
+                img, item_size = self._draw_legend_item(
+                    img, current_position, mark, rank
+                )
+                max_width = max(max_width, item_size.x)
+
+                if grid_pos[0] == rows or (
+                    ((grid_pos[1] - 1) * rows + grid_pos[0]) == n_items
+                ):
+                    size.x += max_width
+
+                if grid_pos[0] + 1 <= rows:
+                    current_position.y += max_height + self.line_space
+                else:
+                    current_position.x += max_width + self.column_space
+                    current_position.y = inner_position.y
+                    max_width = 0
+
+        img = drop_shadow(
+            img, self.shadow_offset, self.shadow_color, self.shadow_iterations
+        )
+        return self._draw_border(img, position, size + 2 * self.inner_offset)
+
+    def _check_height(self, img, marks):
+        draw = ImageDraw.Draw(img)
+        max_height = 0
+        for mark in marks.keys():
+            if mark:
+                _, h = draw.textsize(
+                    mark, font=self.font, stroke_width=self.font_stroke
+                )
+                max_height = max(max_height, h)
+
+        return max_height
+
+    def _draw_legend_item(self, img, position, mark_name, mark_rank):
+        draw = ImageDraw.Draw(img)
+
+        rank_label = {
+            "A1": "A",
+            "A2": "A",
+            "B1": "B",
+            "B2": "B",
+            "S": "S",
+            "SS": "SS",
+            "SSs": "SS",
+        }
+        label = f"{mark_name} ({rank_label[mark_rank]})"
+        _, hc = draw.textsize(
+            "a", font=self.font, stroke_width=1
+        )  # Determine the height of the text's baseline
+        w, h = draw.textsize(label, font=self.font, stroke_width=1)  # Actual text size
+
+        draw.ellipse(
+            [
+                *(position + (0, (1 - self.mark_scale) * 0.5 * hc)),
+                *(position + (self.mark_scale * hc, (1 + self.mark_scale) * 0.5 * hc)),
+            ],
+            fill=self.colors[mark_rank],
+            outline=None,
+            width=0,
+        )
+        draw.text(
+            position + (hc * self.mark_scale * 1.75, 0),
+            label,
+            fill="white",
+            font=self.font,
+            stroke_width=self.font_stroke,
+            stroke_fill="black",
+        )
+
+        size = Position(hc * self.mark_scale * 1.75 + w, h)
+        return img, size
+
+    def _draw_border(self, img, position, size):
+        border = Image.new("RGBA", img.size, (0, 0, 0, 0))
+        draw = ImageDraw.Draw(border)
+
+        draw.rectangle(
+            [*position, *(position + size)],
+            fill=ImageColor.getrgb("#ddbf77") + (127,),
+            outline="#a07d4f",
+            width=3,
+        )
+        draw.rectangle(
+            [*(position + self.space), *(position + size - self.space)],
+            fill=None,
+            outline="#a07d4f",
+            width=1,
+        )
+        border = drop_shadow(
+            border, self.shadow_offset, self.shadow_color, self.shadow_iterations
+        )
+        return Image.alpha_composite(border, img)
